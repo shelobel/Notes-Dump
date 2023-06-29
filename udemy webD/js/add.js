33f18acf566ae2a3e1895ed6c6bdffc6d987a846
shelobel <78535344+shelobel@users.noreply.github.com>
2023-06-29 16:50:50 +0530

    Update add.js

---
+++ udemy webD/js/add.js
@@ -0,0 +1,7 @@
+// basic concepts similar to any other coding lang cpp etc..
+//some new functions
+// array.push(), array.pop(), initialisation of array like ->   arr[] or const arr[];
+// console.log() for logging in console
+// arr.includes("item") //like linear search looks for item
+//string s = arr.slice(2,1); //new arr or new string which collects items from index 2 till a length of 1
+//Math.random(), Math.floor() etc etc
